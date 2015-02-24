f1653821c8683b876039f906bc4b85557a1ede77
Mohamed Ahmed Abdel Fattah <mohamedahmedabdelfattah@gmail.com>
2015-02-24 17:37:00 +0200

    Initial Commit

---
+++ formMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace FxProfile
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowseFolders = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "rar";
+            this.openFileDialog1.FileName = "FxProfileBackup";
+            // 
+            // btnBrowseFolders
+            // 
+            this.btnBrowseFolders.Location = new System.Drawing.Point(392, 271);
+            this.btnBrowseFolders.Name = "btnBrowseFolders";
+            this.btnBrowseFolders.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFolders.TabIndex = 3;
+            this.btnBrowseFolders.Text = "Folder";
+            this.btnBrowseFolders.UseVisualStyleBackColor = true;
+            this.btnBrowseFolders.Visible = false;
+            this.btnBrowseFolders.Click += new System.EventHandler(this.btnBrowseFolders_Click);
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBackup.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackup.ForeColor = System.Drawing.Color.Green;
+            this.btnBackup.Location = new System.Drawing.Point(112, 260);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(134, 42);
+            this.btnBackup.TabIndex = 4;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "rar";
+            this.saveFileDialog1.FileName = "FxProfileBackup";
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRestore.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestore.ForeColor = System.Drawing.Color.Blue;
+            this.btnRestore.Location = new System.Drawing.Point(252, 260);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(134, 42);
+            this.btnRestore.TabIndex = 7;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(474, 206);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.formMain_KeyPress);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(3, 311);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(175, 14);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "mohamed.aabdelfattah@tedata.net";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 14);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "for any inquiry:";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 224);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(474, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(438, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 14);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "v0.19.1.15";
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 331);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnBrowseFolders);
+            this.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FxProfile";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.formMain_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowseFolders;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        public System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ formMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.IO;
+
+namespace FxProfile
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        string FxProfilePath = Environment.ExpandEnvironmentVariables("%APPDATA%\\Mozilla\\Firefox");
+        string FxProfilePathAlt = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\Roaming\Mozilla\Firefox";
+        
+        private void folderPath()
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                FxProfilePath = folderBrowserDialog1.SelectedPath;
+            }
+            else
+                FxProfilePath = FxProfilePathAlt;
+        }
+
+        private void btnBrowseFolders_Click(object sender, EventArgs e)
+        {
+            folderPath();
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+
+            saveFileDialog1.Filter = "FxProfile files (*.fxbu)|*.fxbu";
+
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                if ((File.Exists(saveFileDialog1.FileName)))
+                {
+                    File.Delete(saveFileDialog1.FileName);
+                }
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+
+            openFileDialog1.Filter = "FxProfile files (*.fxbu)|*.fxbu";
+
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:mohamed.aabdelfattah@tedata.net?subject=FxProfile "+label2.Text);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            btnBackup.Enabled = false;
+            btnRestore.Enabled = false;
+            timer1.Enabled = true;
+
+            try
+            {
+                ZipFile.CreateFromDirectory(FxProfilePath, saveFileDialog1.FileName);
+                progressBar1.Value = 100;
+            }
+            catch (Exception ex)
+            {
+                timer1.Enabled = false;
+                progressBar1.Value = 0;
+                MessageBox.Show(ex.ToString());
+
+                if ((File.Exists(saveFileDialog1.FileName)))
+                {
+                    File.Delete(saveFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            timer1.Enabled = false;
+
+            if (progressBar1.Value == 100)
+                MessageBox.Show("Backup created successfully");
+            else
+                progressBar1.Value = 0;
+
+            btnBackup.Enabled = true;
+            btnRestore.Enabled = true;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void formMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'f')
+                folderPath();
+            else if (e.KeyChar == 'p')
+            {   
+                //Test
+                MessageBox.Show(FxProfilePath);
+                MessageBox.Show(FxProfilePathAlt);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value != 100)
+                progressBar1.Value += 1;
+
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            this.Focus();
+            richTextBox1.Focus();
+        }
+
+        private void backgroundWorker2_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            btnBackup.Enabled = false;
+            btnRestore.Enabled = false;
+            timer1.Enabled = true;
+
+            try
+            {
+                if (!Directory.Exists(FxProfilePath))
+                {
+                    Directory.CreateDirectory(FxProfilePath);
+                }
+
+                ZipFile.ExtractToDirectory(openFileDialog1.FileName, FxProfilePath);
+                progressBar1.Value = 100;
+            }
+            catch (Exception ex)
+            {
+                timer1.Enabled = false;
+                progressBar1.Value = 0;
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            timer1.Enabled = false;
+
+            if(progressBar1.Value == 100)
+                MessageBox.Show("Data restored successfully");
+            else
+                progressBar1.Value = 0;
+
+            btnBackup.Enabled = true;
+            btnRestore.Enabled = true;
+        }
+    }
+}
